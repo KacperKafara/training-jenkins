9476686c2d81d06968d69b055c666691366f06d2
PiotrPleska <116757969+PiotrPleska@users.noreply.github.com>
2024-07-10 11:32:01 +0200

    Update plugin.groovy

--- vars/plugin.groovy
+++ vars/plugin.groovy
@@ -1,5 +1,5 @@
 def trivy_scan(String imageName) {
-                 sh 'docker run --name ${imageName} -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.53.0 image training_app -f template --template "@contrib/html.tpl" -o /report.html'
+                sh 'docker run --name ${imageName} -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.53.0 image training_app -f template --template "@contrib/html.tpl" -o /report.html'
                 sh 'docker cp ${imageName}:/report.html .'
                 sh 'docker rm ${imageName}'
                 sh 'cat report.html'
