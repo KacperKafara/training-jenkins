1c0f7908ce652693a7cf12bdd708b3adfc83084e
Jakub Kozielski <jakubkoz@corp.idemia.com>
2024-07-19 10:04:44 +0200

    Changed lb_rule outbound use snat

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,6 +108,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   frontend_ip_configuration_name = azurerm_lb.load_balancer.frontend_ip_configuration[0].name
   backend_address_pool_ids = [azurerm_lb_backend_address_pool.backend_pool.id]
   probe_id = azurerm_lb_probe.probe.id
+  disable_outbound_snat          = true
 }
 
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
