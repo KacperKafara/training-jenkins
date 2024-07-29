783f7ffbc7afbd8de0dd75cf4af51a1b5948c6c1
Mateusz Milczarek <mateumil@corp.idemia.com>
2024-07-29 09:48:29 +0200

    Storage resource to data

--- terraform/main.tf
+++ terraform/main.tf
@@ -202,19 +202,35 @@ module "monitoring" {
 }
 
 # Create storage account wiht blob container for loki logs storage
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "storageaccount"
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# resource "azurerm_storage_account" "storage_account" {
+#   name                     = "${azurerm_resource_group.resource_group.name}storageaccountlogs" 
+#   resource_group_name      = azurerm_resource_group.resource_group.name
+#   location                 = azurerm_resource_group.resource_group.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# resource "azurerm_storage_container" "storage_container" {
+#   name                  = "log-container"
+#   storage_account_name  = azurerm_storage_account.storage_account.name
+#   container_access_type = "private"
+# }
+
+data "azurerm_resource_group" "resource_group_keyvault" {
+  name = "group1-keyvault"
 }
 
-resource "azurerm_storage_container" "storage_container" {
-  name                  = "log-container"
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = "private"
+
+data "azurerm_storage_account" "storage_account" {
+  name                = "group1storageaccountlogs"
+  resource_group_name = azurerm_resource_group.resource_group_keyvault.name
 }
+
+data "azurerm_storage_container" "storage_container" {
+  name                 = "log-container"
+  storage_account_name = data.azurerm_storage_account.storage_account.name
+}
+
 
 #=====================================================
 
